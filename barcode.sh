80aec810e1cfb1c2f60dd5dd0034b803ced13232
Matthew Knox <matthew@makereti.co.nz>
2025-03-19 23:56:27 +1100

    Add images

--- barcode.sh
+++ barcode.sh
@@ -6,4 +6,4 @@ directory=`dirname "$1"`
 filename=`basename "$1" | rev | cut -f 2- -d '.' | rev`
 extension=`echo "$1" | rev | cut -f 1 -d '.' | rev`
 
-ffmpeg -i "$1" -filter:v "scale=1:150" -r 1 -f image2pipe -c:v ppm - | convert '(' '+append' - ')' -resize 500x150! "$output/$filename.png"
+ffmpeg -i "$1" -filter:v "scale=1:150:out_color_matrix=bt601:out_range=pc" -r 1 -f image2pipe -c:v ppm - | convert '(' '+append' - ')' -resize 500x150! "$output/$filename.png"
